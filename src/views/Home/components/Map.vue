1210bbde2f20ea587331938454eb6c86e52784cf
lulu <xinhua.lulu@gmail.com>
2020-02-19 12:42:07 +0800

    leaflet 改為原生不使用 vue 套件

--- src/views/Home/components/Map.vue
+++ src/views/Home/components/Map.vue
@@ -1,44 +1,41 @@
 <template>
   <div id="map">
-    <l-map
-      :zoom="zoom"
-      :center="center">
-      <l-tile-layer :url="OSMUrl"></l-tile-layer>
-      <l-marker :lat-lng="markerLatlng"></l-marker>
-    </l-map>
   </div>
 </template>
 
 <script>
-// import L from 'leaflet';
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import L from 'leaflet';
 
 export default {
   name: 'appMap',
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-  },
   data() {
     return {
       zoom: 18,
-      center: [22.604799, 120.2976256],
-      markerLatlng: [22.604799, 120.2976256],
+      center: [],
+      markerLatlng: [],
       OSMUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     };
   },
-  created() {
-    // 存取當前位置
-    if (!navigator.geolocation) {
-      alert('你的瀏覽器不允許存取所在位置');
-      return;
-    }
-    navigator.geolocation.getCurrentPosition((position) => {
+  methods: {
+    getMap(position) {
       this.center = [position.coords.latitude, position.coords.longitude];
-      this.markerLatlng = this.center;
-    });
-    // TODO: 製作存取位置通知與存取失敗通知
+      const map = L.map('map', {
+        center: this.center,
+        zoom: this.zoom,
+      });
+
+      L.tileLayer(this.OSMUrl, {
+        attribution: '作者: LuU 昀芷 | UI 參考: https://reurl.cc/oDlokQ',
+      }).addTo(map);
+    },
+  },
+  mounted() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.getMap);
+      // TODO: 製作存取位置通知與存取失敗通知
+    } else {
+      alert('你的瀏覽器不允許存取所在位置');
+    }
   },
 };
 </script>
